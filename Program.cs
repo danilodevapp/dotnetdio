b7ef516d146c8e79d778b60b0830c11f9de1f119
danilodevapp <danilodevapp@gmail.com>
2023-09-05 13:04:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using diointer.Models;
+
+Pessoa p1;
+
+p1 = new Pessoa();
+
+Console.WriteLine();
+
+Console.Write("Digite o Nome: ");
+p1.Nome = Console.ReadLine();
+
+Console.WriteLine();
+
+Console.Write("Digite a idade: ");
+p1.Idade = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+// p1.Nome = "Danilo";
+// p1.Idade = 44;
+
+p1.Apresentar();
